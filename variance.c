4637974aca42d4a04c36fb8fe9e040bfd209adb1
carasiae <muhluqhakim@gmail.com>
2025-04-08 15:50:25 +0700

    feat: add average function + fix function signature

--- variance.c
+++ variance.c
@@ -1,19 +1,38 @@
 #include "variance.h"
+#include <omp.h>
 
 static unsigned int seed = 100;
-RGB average(const unsigned char *rgb_data, int row, int col, int width, int height){
-    RGB res = {0,0,0};
+RGB average(const unsigned char *rgb_data, int row_start, int col_start, int row_end,
+            int col_end,int width, int height){
+    RGB res;
+    double r, g, b;
+    #pragma omp parallel for reduction(+: r, g, b)
+    for (int i = row_start; i <= row_end; i++){
+        for (int j = col_start; j <= col_end; j++){
+            r += (double) rgb_data[(i*width+j)*3];
+            g += (double) rgb_data[(i*width+j)*3+1];
+            b += (double) rgb_data[(i*width+j)*3+2];
+        }
+    }
+    int area = (row_end - row_start + 1) * (col_end - col_start + 1);
+    res.r = (unsigned char) (r / area);
+    res.g = (unsigned char) (g / area);
+    res.b = (unsigned char) (b / area);
     return res;
 }
-double variance(const unsigned char *rgb_data, int row, int col, int width, int height){
+double variance(const unsigned char *rgb_data, int row_start, int col_start, int row_end,
+            int col_end,int width, int height) {
     return 1;
 }
-double mean_absolute_deviation(const unsigned char *rgb_data, int row, int col, int width, int height){
+double mean_absolute_deviation(const unsigned char *rgb_data, int row_start, int col_start, int row_end,
+            int col_end,int width, int height) {
     return 1;
 }
-double max_pixel_difference(const unsigned char *rgb_data, int row, int col, int width, int height){
+double max_pixel_difference(const unsigned char *rgb_data, int row_start, int col_start, int row_end,
+            int col_end,int width, int height) {
     return 1;
 }
-double entropy(const unsigned char *rgb_data, int row, int col, int width, int height){
+double entropy(const unsigned char *rgb_data, int row_start, int col_start, int row_end,
+            int col_end,int width, int height) {
     return 1;
 }
--- variance.h
+++ variance.h
@@ -2,12 +2,44 @@
 #define ERROR_MEASUREMENT_H
 #include "qtree.h"
 
-typedef double (*VarianceFunction)(const unsigned char *, int row, int col, int width, int height);
+/** Variance function used to determine the variance of an area in an image
+    @param rgb_data     pixels of an image, following stb_image's format
+    @param row_start    starting row of the area to be calculated
+    @param col_start    starting column of the area to be calculated
+    @param row_end      ending row of the area to be calculated
+    @param col_end      ending column of the area to be calculated
+    @param width        width of the image
+    @param height       height of the image
+    @return             the variance value
+*/
+typedef double (VarianceFunction)(const unsigned char *rgb_data,
+                                  int row_start,
+                                  int col_start,
+                                  int row_end,
+                                  int col_end,
+                                  int width,
+                                  int height);
 
-RGB average(const unsigned char *rgb_data, int row, int col, int width, int height);
-double variance(const unsigned char *rgb_data, int row, int col, int width, int height);
-double mean_absolute_deviation(const unsigned char *rgb_data, int row, int col, int width, int height);
-double max_pixel_difference(const unsigned char *rgb_data, int row, int col, int width, int height);
-double entropy(const unsigned char *rgb_data, int row, int col, int width, int height);
+VarianceFunction variance;
+VarianceFunction mean_absolute_deviation;
+VarianceFunction max_pixel_difference;
+VarianceFunction entropy;
 
+/** Average (mean) RGB value of an area of an area in an image
+    @param rgb_data     pixels of an image, following stb_image's format
+    @param row_start    starting row of the area to be calculated
+    @param col_start    starting column of the area to be calculated
+    @param row_end      ending row of the area to be calculated
+    @param col_end      ending column of the area to be calculated
+    @param width        width of the image
+    @param height       height of the image
+    @return             the average value
+*/
+RGB average_rgb(const unsigned char *rgb_data,
+                int row_start,
+                int col_start,
+                int row_end,
+                int col_end,
+                int width,
+                int height);
 #endif
